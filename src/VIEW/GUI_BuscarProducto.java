bd100dbb87f6e12504090585bcbe660a183d1e16
juanm <juanm@DESKTOP-LRIBG53>
2019-11-11 19:35:59 -0600

    AGREGUE MÁS GUIS

--- src/VIEW/GUI_BuscarProducto.java
+++ src/VIEW/GUI_BuscarProducto.java
@@ -118,10 +118,8 @@ public class GUI_BuscarProducto extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUI_BuscarProducto().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUI_BuscarProducto().setVisible(true);
         });
     }
 
--- src/VIEW/GUI_GestorUsuariosADMIINISTRADOR.java
+++ src/VIEW/GUI_GestorUsuariosADMIINISTRADOR.java
@@ -32,13 +32,13 @@ public class GUI_GestorUsuariosADMIINISTRADOR extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("GestorUsuariosAdmin");
         setMaximumSize(new java.awt.Dimension(530, 410));
         setMinimumSize(new java.awt.Dimension(530, 410));
-        setPreferredSize(new java.awt.Dimension(530, 410));
         setResizable(false);
 
         jPanel2.setMaximumSize(new java.awt.Dimension(530, 410));
@@ -52,7 +52,7 @@ public class GUI_GestorUsuariosADMIINISTRADOR extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 208, 32));
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, 130, 32));
 
         jLabel1.setBackground(new java.awt.Color(255, 0, 51));
         jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
@@ -60,15 +60,25 @@ public class GUI_GestorUsuariosADMIINISTRADOR extends javax.swing.JFrame {
         jLabel1.setText("Administrar Usuarios");
         jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 30, 258, 57));
 
-        jButton3.setText("Buscar Usuario");
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 200, 208, 32));
+        jButton3.setText("Visualizar Usuarios");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 190, 130, 32));
 
         jButton5.setText("Regresar");
         jPanel2.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 310, 208, 35));
 
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("Buscar Usuario");
+        jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 190, 120, 32));
+
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/adm.png"))); // NOI18N
-        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 530, 410));
+        jLabel2.setMaximumSize(new java.awt.Dimension(530, 410));
+        jLabel2.setMinimumSize(new java.awt.Dimension(530, 410));
+        jLabel2.setPreferredSize(new java.awt.Dimension(530, 410));
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -91,6 +101,10 @@ public class GUI_GestorUsuariosADMIINISTRADOR extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,16 +134,15 @@ public class GUI_GestorUsuariosADMIINISTRADOR extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUI_GestorUsuariosADMIINISTRADOR().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUI_GestorUsuariosADMIINISTRADOR().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
